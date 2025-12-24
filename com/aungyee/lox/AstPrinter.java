8322d3ede9660eeef173591b648d026de2f07a09
Aung Yee <lyndon.lso@gmail.com>
2025-12-24 00:45:37 +0000

    Finished Assignment Expression

--- com/aungyee/lox/AstPrinter.java
+++ com/aungyee/lox/AstPrinter.java
@@ -31,6 +31,11 @@ class AstPrinter implements Expr.Visitor<String> {
         return expr.name.toString();
     }
 
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return expr.name.toString() + " = " + expr.value.toString();
+    }
+
 	private String parenthesize(String name, Expr... exprs) {
 		StringBuilder builder = new StringBuilder();
 
